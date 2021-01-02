89579b3dad91df2c7d04c8576ebaa3dbab13ed10
Jeroen van Gemert <jeroen.vangemert@kpn.com>
2021-01-02 17:54:29 +0100

    les_5_13

--- lessons/les_5_12.py
+++ lessons/les_5_12.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Detect distance with ultrasonic sensor
+Control stepper motor
 
 Position of switches:
 +---+--------+--------+
---
+++ lessons/les_5_13.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Control servo motor
+
+Position of switches:
++---+--------+--------+
+|on |        |      78|
+|off|        |        |
++---+-----------------+
+
+Positive values rotate clockwise
+Negative values rotate counterclockwise
+"""
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+class sg90:
+  def __init__( self, pin):
+    GPIO.setmode( GPIO.BOARD )
+    GPIO.setup( pin, GPIO.OUT )
+    self.pin = int( pin )
+    self.direction = 0
+    self.servo = GPIO.PWM( self.pin, 50 )
+    self.servo.start(0.0)
+
+  def cleanup( self ): #Funktion zum Stoppen und GPIO Pins Freigeben
+    self.servo.ChangeDutyCycle(self._henkan(0))
+    time.sleep(0.3)
+    self.servo.stop()
+    GPIO.cleanup()
+
+  def currentdirection( self ): #Funktion die die Momentane Position feststellt.
+    return self.direction
+
+  def _henkan( self, value ):
+    return 0.05 * value + 7.0
+
+  def setdirection( self, direction, speed ): #Funktion um die Richtung anzugeben
+
+    for d in range( self.direction, direction, int(speed) ):
+      self.servo.ChangeDutyCycle( self._henkan( d ) )
+      self.direction = d
+      time.sleep(0.1)
+    self.servo.ChangeDutyCycle( self._henkan( direction ) )
+    self.direction = direction
+
+def main():
+    servo_pin = 22
+    s = sg90(servo_pin) #Deklaration von Pin und Motor
+    try:
+        while True:
+            print ("Turn left ...")
+            s.setdirection( 80, 100 )   #Links Herum drehen
+            time.sleep(0.5)             #0,5 Sekunden warten
+            s.setdirection( 50, 100 )   #Links Herum drehen
+            time.sleep(0.5)             #0,5 Sekunden warten
+            s.setdirection( 50, 100 )   #Links Herum drehen
+            time.sleep(0.5)             #0,5 Sekunden warten
+            print ("Turn right ...")
+            s.setdirection( -80, 100 ) #Rechts Herum drehen
+            time.sleep(01.5)             #0,5 Sekunden warten
+            s.setdirection( -50, 100 ) #Rechts Herum drehen
+            time.sleep(01.5)             #0,5 Sekunden warten
+    except KeyboardInterrupt:
+        s.cleanup()
+
+if __name__ == "__main__":
+    main()
