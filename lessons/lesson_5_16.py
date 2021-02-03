dd5ea1d63166dcda413ef9e4cedbe35041a1111c
Jeroen van Gemert <jeroen.vangemert@kpn.com>
2021-02-03 20:42:40 +0100

    Added lesson

---
+++ lessons/lesson_5_16.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Control 7 segement display
+
+Position of switches:
++---+--------+--------+
+|on |        | 2      |
+|off|        |        |
++---+-----------------+
+"""
+
+import time
+import RPi.GPIO as GPIO
+
+#define tilt_pin
+tilt_pin = 15
+
+#set GPIO mode to GPIO.BOARD
+GPIO.setmode(GPIO.BOARD)
+
+# set pin as INPUT
+GPIO.setup(tilt_pin, GPIO.IN)
+
+try:
+    while True:
+        #positive is tilt to the left / negative is tilt to the right
+        if GPIO.input(tilt_pin):
+            print ("[-] Left Tilt")
+        else:
+            print ("[-] Right Tilt")
+        time.sleep(1)
+except KeyboardInterrupt:
+    #CTRL+C exists program
+    GPIO.cleanup()
